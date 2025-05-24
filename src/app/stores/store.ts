5c1d752a4160ae8f1e41e6693b67f1f7439fb435
Kinh Kha <khadoan.190809@gmail.com>
2025-05-24 16:41:41 +0700

    change

---
+++ src/app/stores/store.ts
@@ -0,0 +1,14 @@
+// import { create } from 'zustand'
+// import { createCartSlice, CartSlice } from './cartSlice'
+// import { createProductSlice, ProductSlice } from './productSlice'
+// import { createAuthSlice, AuthSlice } from './authSlice'
+
+// type StoreState = CartSlice & ProductSlice & AuthSlice
+
+// const useStore = create<StoreState>()((set, get) => ({
+//   ...createCartSlice(set, get),
+//   ...createProductSlice(set),
+//   ...createAuthSlice(set),
+// }))
+
+// export default useStore
