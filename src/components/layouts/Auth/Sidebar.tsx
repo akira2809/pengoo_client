c1db0015372810ef8ecddcf23c719467826a6a7e
Mình Phuoc Le <radensparks2507@gmail.com>
2025-06-27 14:05:51 +0700

    Temporary Voucher Page in User Menu

--- src/components/layouts/Auth/Sidebar.tsx
+++ src/components/layouts/Auth/Sidebar.tsx
@@ -11,6 +11,7 @@ const menuItems = [
   { name: 'Địa chỉ giao hàng', href: '/account/addresses' },
   { name: 'Đổi mật khẩu', href: '/account/change-password' },
   { name: 'Yêu thích', href: '/account/wishlist' },
+  { name: 'Voucher của tôi', href: '/account/vouchers' },
 ];
 
 export function Sidebar() {
