30592db08efdd35524802bd04ece5db618e0889f
akira2809 <146043171+akira2809@users.noreply.github.com>
2025-07-11 12:49:13 +0700

    Update page.tsx

--- src/app/(public)/collection/[slug]/page.tsx
+++ src/app/(public)/collection/[slug]/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useStore } from '@/app/stores/store';
 import { productService } from '@/app/api/services/productService';
-import { ProductPageLayout } from '@/app/(public)/product/component/ProductPageLayout';
+import { ProductPageLayout } from '@/app/(public)/products/component/ProductPageLayout';
 import { useRouter } from 'next/navigation';
 import { Product } from '@/app/stores/type';
 
