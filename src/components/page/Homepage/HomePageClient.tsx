0a14ef3a16b9835e2c29c137ae6ed0395313736b
akira2809 <146043171+akira2809@users.noreply.github.com>
2025-07-22 21:13:28 +0700

    Update HomePageClient.tsx

--- src/components/page/Homepage/HomePageClient.tsx
+++ src/components/page/Homepage/HomePageClient.tsx
@@ -106,7 +106,6 @@ function HomePage() {
     <>
       <Suspense fallback={<div className="h-[500px] w-full bg-gray-100 animate-pulse" />}>
         <Banner />
-        <h1>hihi</h1>
       </Suspense>
 
       <section className="py-12 sm:py-20">
