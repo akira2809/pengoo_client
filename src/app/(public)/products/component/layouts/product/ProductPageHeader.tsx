49d9ad73259906d71cca11910f124478e021fc1b
huynv2005s <nguyenvuuhuy2005s@gmail.com>
2025-08-05 14:33:15 +0700

    fix color box show total products

--- src/app/(public)/products/component/layouts/product/ProductPageHeader.tsx
+++ src/app/(public)/products/component/layouts/product/ProductPageHeader.tsx
@@ -24,18 +24,26 @@ export const ProductPageHeader: React.FC<ProductPageHeaderProps> = ({
   totalFilteredProducts,
   totalProducts
 }) => {
+
+  const color = totalFilteredProducts === 0 ? 'yellow' : 'green';
   return (
     <div className="flex justify-between items-center mb-6 flex-wrap gap-4">
       {/* Thông báo số lượng sản phẩm */}
-      <div className="w-full lg:w-auto order-last lg:order-first mb-4 lg:mb-0 p-4 bg-yellow-50 border-l-4 border-yellow-400">
+      <div className={`w-full lg:w-auto order-last lg:order-first mb-4 lg:mb-0 p-4 bg-${color}-50 border-l-4 border-${color}-400`}>
         <div className="flex items-center">
           <div className="flex-shrink-0">
-            <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-            </svg>
+            {
+              totalFilteredProducts === 0 ? <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+              </svg> :
+                <svg className="h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414L9 13.414l4.707-4.707z" clipRule="evenodd" />
+                </svg>
+
+            }
           </div>
           <div className="ml-3">
-            <p className="text-sm text-yellow-700">
+            <p className={`text-sm text-${color}-700`}>
               Đang hiển thị {totalFilteredProducts} sản phẩm (Tổng: {totalProducts})
             </p>
           </div>
@@ -67,8 +75,7 @@ export const ProductPageHeader: React.FC<ProductPageHeaderProps> = ({
                   <Listbox.Option
                     key={option.id}
                     className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
                       }`
                     }
                     value={option}
