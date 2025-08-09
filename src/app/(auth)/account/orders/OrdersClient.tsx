fc8fcfc316d5a0a0d4f5f306d4a53480e10d6a6a
akira2809 <146043171+akira2809@users.noreply.github.com>
2025-08-09 19:35:39 +0700

    Update OrdersClient.tsx

--- src/app/(auth)/account/orders/OrdersClient.tsx
+++ src/app/(auth)/account/orders/OrdersClient.tsx
@@ -13,6 +13,8 @@ export default function OrdersClient() {
   return (
     <Suspense fallback={<div className="text-center py-12">Đang tải...</div>}>
       <OrdersContent />
+      <p>1</p>
     </Suspense>
+    
   );
 }
